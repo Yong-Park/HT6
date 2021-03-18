9cac10acdc1841a39198adf09310fb65b5802660
Yong <yong.park123456@gmail.com>
2021-03-18 08:00:33 -0600

    Ideas basicas para comenzar el trabajo

---
+++ Factory.java
@@ -0,0 +1,12 @@
+public class Factory {
+    public GeneralMap FactorySelection(int data){
+        if(data == 0){
+            
+        }else if(data == 1){
+
+        }else if(data == 2){
+
+        }
+        return null;
+    }  
+}
---
+++ Main.java
@@ -0,0 +1,3 @@
+public class Main {
+    //pedir cual de los tres maps desea aplicar y luego que escoga realizar la lectura del archivo txt
+}
